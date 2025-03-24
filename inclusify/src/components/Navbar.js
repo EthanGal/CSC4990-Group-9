afaf6ceb9ed5b09798f745fae17d225047ed7e1e
ebarraza01 <115087980+ebarraza01@users.noreply.github.com>
2025-03-24 09:53:18 -0500

    Navbar Item Fix
    Separates jumbled navbar item, and gives each its proper link.

--- inclusify/src/components/Navbar.js
+++ inclusify/src/components/Navbar.js
@@ -10,9 +10,9 @@ const Navbar = () => (
 
             <div className="d-flex me-4">
             <Link className="nav-link me-2" to="/">Home</Link>
-                <Link className="nav-link" to="/reports">Reports</Link>
-                <Link className="nav-link" to="/login">Login</Link>
-                <Link className="nav-link" to="/scannedsites">Scanned Sites</Link> {/*TODO: hide this option until user is logged in.*/}
+            <Link className="nav-link me-2" to="/reports">Reports</Link>
+            <Link className="nav-link me-2" to="/login">Login</Link>
+            <Link className="nav-link me-2" to="/scannedsites">Scanned Sites</Link> {/*TODO: hide this option until user is logged in.*/}
             </div>
         </div>
     </nav>
